ee6c719a3c0c96be912743da7846ff36bf1210e1
reokoizumi <reokoizumi0930@gmail.com>
2022-07-18 23:20:29 +0900

    add: router

--- src/routers/routers.go
+++ src/routers/routers.go
@@ -1,5 +1,19 @@
 package routers
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/reokoizumi/todo-gin/controllers"
+)
 
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	app := r.Group("/app")
+	{
+		app.GET("todo", controllers.GetTodos)
+		app.POST("todo", controllers.CreateATodo)
+	}
+	return r
+}
 
 
