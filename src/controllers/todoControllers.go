47e14e599cf4f3fb1576b19b3e923b92d8337397
reokoizumi <reokoizumi0930@gmail.com>
2022-07-18 23:20:23 +0900

    add: all get and post method in controller

--- src/controllers/todoControllers.go
+++ src/controllers/todoControllers.go
@@ -1,3 +1,32 @@
 package controllers
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/reokoizumi/todo-gin/models"
+)
+
+
+func GetTodos(c *gin.Context) {
+	var todo []models.Todo
+	err := models.GetAllTodos(&todo)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
+func CreateATodo(c *gin.Context) {
+	var todo models.Todo
+	c.BindJSON(&todo)
+	err := models.CreateATodo(&todo)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
 
